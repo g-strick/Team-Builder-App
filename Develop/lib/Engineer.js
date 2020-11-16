e21c9058dfbab29a4e776b43fde1ff0ae5b145c4
G Strick <graysonstricker@gmail.com>
2020-11-16 14:18:16 -0500

    updated Engineer constructor

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+}
+
+// test
+// const engineer = new Engineer("bob", 123, "bob@aol.com", "bobbycodes");
+// console.log("engineer:", engineer);
